3fecaa230279fb87c51ebede22950ffe2e3eaee8
Dikasteria <rjhey@hotmail.com>
2021-06-06 12:02:56 +0100

    add item to db and create card to represent info via map function

--- dynamo-app/src/App.tsx
+++ dynamo-app/src/App.tsx
@@ -4,6 +4,8 @@ import Amplify, { API, graphqlOperation } from "aws-amplify";
 import awsconfig from "./aws-exports";
 import {AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
 import { listSongs } from './graphql/queries';
+import { IconButton, Paper } from '@material-ui/core';
+import { FavoriteOutlined, PlayArrow } from '@material-ui/icons';
 
 Amplify.configure(awsconfig);
 
@@ -11,12 +13,12 @@ type getSongsQuery = {
   listSongs: {
     items: {[key: string]: any}[];
   }
-}
+};
 
 function App() { 
 
   const [songList, setSongList] = useState< {[key: string]: any}[]>([]);
-
+  console.log(songList)
   const fetchSongs = async () => {
     try {
       const songData = (await API.graphql(graphqlOperation(listSongs))) as {
@@ -36,10 +38,42 @@ function App() {
 
   return (
     <div className="App">
-        <header className="app-header">
+        <header className="App-header">
           <AmplifySignOut />
           <h2>My App Conent!</h2>
         </header>
+        <div className="songList">
+          {songList.map(song => {
+            return (
+              <Paper variant="outlined" elevation={2} >
+                <div className="songCard">
+                  <IconButton aria-label="play">
+                    <PlayArrow/>
+                  </IconButton>
+                </div>
+              <div>
+                <div className="songTitle">
+                  {song.name}
+                </div>
+                <div className="songOwner">
+                  {song.owner}
+                </div>
+                <div>
+                  <IconButton aria-label="like">
+                    <FavoriteOutlined />
+                    &nbsp; {song.like}
+                  </IconButton>
+                </div>
+                <div>
+                  <div className="songDescription">
+                    {song.description} 
+                  </div>
+                </div>
+              </div>
+              </Paper>
+            )
+          })}
+        </div>
     </div>
   );
 }
