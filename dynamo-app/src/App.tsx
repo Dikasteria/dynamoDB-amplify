8ebdb524340353eb2bbcc3a2964c31a04ded0fe0
Dikasteria <rjhey@hotmail.com>
2021-06-06 12:40:11 +0100

    add function to increase likes using updateSong mutation

--- dynamo-app/src/App.tsx
+++ dynamo-app/src/App.tsx
@@ -4,6 +4,7 @@ import Amplify, { API, graphqlOperation } from "aws-amplify";
 import awsconfig from "./aws-exports";
 import {AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
 import { listSongs } from './graphql/queries';
+import { updateSong } from './graphql/mutations';
 import { IconButton, Paper } from '@material-ui/core';
 import { FavoriteOutlined, PlayArrow } from '@material-ui/icons';
 
@@ -14,6 +15,10 @@ type getSongsQuery = {
     items: {[key: string]: any}[];
   }
 };
+
+type updateSongQuery = {
+  updateSong: {[key: string]: any},
+}
 
 function App() { 
 
@@ -30,11 +35,30 @@ function App() {
       console.log("Error pulling data: ", error)
     }
 
-  }
+  };
 
   useEffect(() => {
     fetchSongs();
-  }, [])
+  }, []);
+  
+  const addLike = async (index: number) => {
+    
+    try {
+      const song = songList[index];
+      song.like = song.like+1;
+      delete song.createdAt;
+      delete song.updatedAt;
+      const songData = await API.graphql(graphqlOperation(updateSong, {input: song})) as {
+        data: updateSongQuery
+      };
+      const songsList = [...songList];
+      songsList[index] = songData.data.updateSong;
+      setSongList(songsList)
+
+    } catch (error) {
+      console.log("Error on adding like to song: ", error)
+    }
+  }
 
   return (
     <div className="App">
@@ -43,33 +67,33 @@ function App() {
           <h2>My App Conent!</h2>
         </header>
         <div className="songList">
-          {songList.map(song => {
+          {songList.map((song, index) => {
             return (
               <Paper variant="outlined" elevation={2} >
                 <div className="songCard">
                   <IconButton aria-label="play">
                     <PlayArrow/>
                   </IconButton>
+                  {/* <div> */}
+                    <div className="songTitle">
+                      {song.name}
+                    </div>
+                    <div className="songOwner">
+                      {song.owner}
+                    </div>
+                    <div>
+                      <IconButton onClick={() => addLike(index)} aria-label="like">
+                        <FavoriteOutlined />
+                        &nbsp; {song.like}
+                      </IconButton>
+                    </div>
+                    <div>
+                      <div className="songDescription">
+                        {song.description} 
+                      </div>
+                    </div>
+                  {/* </div> */}
                 </div>
-              <div>
-                <div className="songTitle">
-                  {song.name}
-                </div>
-                <div className="songOwner">
-                  {song.owner}
-                </div>
-                <div>
-                  <IconButton aria-label="like">
-                    <FavoriteOutlined />
-                    &nbsp; {song.like}
-                  </IconButton>
-                </div>
-                <div>
-                  <div className="songDescription">
-                    {song.description} 
-                  </div>
-                </div>
-              </div>
               </Paper>
             )
           })}
